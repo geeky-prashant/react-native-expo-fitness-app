57e73dfccbd9622bf8a20448dda8b2eb82cad5db
Prashant Bhardwaj <geekyprashant840@gmail.com>
2024-06-30 20:57:18 +0530

    Configured nativewind

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+export default function index() {
+  return (
+    <>
+      <View>
+        <Text className="text-2xl text-red-500 font-bold px-5 py-4">Hello</Text>
+      </View>
+    </>
+  )
+}
